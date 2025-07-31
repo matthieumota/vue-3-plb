e18c2cc9ce2ce020e300bbad7636824bec97975c
Matthieu Mota <matthieumota@gmail.com>
2025-07-31 14:27:59 +0200

    lifecycle

--- 02-vite/src/pages/About.vue
+++ 02-vite/src/pages/About.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, onUpdated, ref, watch } from 'vue'
+
+const date = ref(new Date())
+let interval: number
+
+console.log(document.querySelector('h1'))
+onMounted(() => {
+  console.log(document.querySelector('h1'))
+  interval = setInterval(() => {
+    console.log('tick')
+    date.value = new Date()
+  }, 10000)
+})
+
+const test = ref(0)
+
+onUpdated(() => {
+  console.log('updated', test)
+})
+
+watch(test, () => {
+  console.log('watch', test)
+})
+
+onUnmounted(() => {
+  clearInterval(interval)
+})
+</script>
+
 <template>
-  <h1>A propos</h1>
+  <h1 @click="test++">A propos</h1>
+  <p>{{ date }}</p>
 </template>
